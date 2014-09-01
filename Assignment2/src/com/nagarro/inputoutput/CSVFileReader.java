ecfe2c37a366334786a16fb3614504173e8337fe
rishabgandhar <rishabgandhar@gmail.com>
2014-09-01 10:19:45 +0530

    Changes

--- Assignment2/src/com/nagarro/inputoutput/CSVFileReader.java
+++ Assignment2/src/com/nagarro/inputoutput/CSVFileReader.java
@@ -25,6 +25,7 @@ import com.nagarro.model.Flight;
 import com.nagarro.service.FlightService;
 import com.nagarro.service.FlightServiceImpl;
 import com.nagarro.util.DateParser;
+import com.nagarro.util.FlightDataValidation;
 
 /**
  * The Class CSVFileReader.
@@ -100,12 +101,12 @@ public class CSVFileReader {
 						rowNo++;
 						String airlineData[] = line.split(SPLITTER);
 						try {
-							if (validFlightData(airlineData, rowNo, airlineName)) {
+							if (FlightDataValidation.validFlightData(
+									airlineData, rowNo, airlineName)) {
 								for (int index = 0; index < airlineData[CLASS_INDEX]
 										.length(); index++) {
 									Flight flight = getInstance(airlineData,
 											index);
-									// if()
 									flights.add(flight);
 								}
 							}
@@ -139,55 +140,5 @@ public class CSVFileReader {
 			isValidAndModified = true;
 		}
 		return isValidAndModified;
-	}
-
-	/**
-	 * Valid flight data.
-	 *
-	 * @param airline
-	 *            the airline
-	 * @param rowNo
-	 *            the row no
-	 * @param airlineName
-	 *            the airline name
-	 * @return true, if successful
-	 * @throws Exception
-	 *             the exception
-	 */
-	public boolean validFlightData(String airline[], int rowNo,
-			String airlineName) throws Exception {
-		boolean isValid = true;
-		if (airline[0].length() != 5) {
-			isValid = false;
-		}
-		if (airline[1].length() != 3 || airline[2].length() != 3) {
-			isValid = false;
-		}
-		try {
-			DateParser.parseDate(airline[3]);
-		} catch (ParseException e) {
-			isValid = false;
-			e.printStackTrace();
-		}
-		try {
-			Double.parseDouble(airline[5]);
-			Double.parseDouble(airline[6]);
-		} catch (NumberFormatException e) {
-			isValid = false;
-			e.printStackTrace();
-		}
-		if (!(airline[7].contains(GenericConstants.YES_OPTION) || airline[7]
-				.contains(GenericConstants.NO_OPTION))) {
-			isValid = false;
-		}
-		if (!(airline[8].contains(CSVReaderConstants.BUSINESS) || airline[8]
-				.contains(CSVReaderConstants.ECONOMIC))) {
-			isValid = false;
-		}
-		if (!isValid) {
-			throw new Exception(ConsoleMessages.INVAILD_FLIGHT_DETAILS + rowNo
-					+ " in " + airlineName);
-		}
-		return isValid;
 	}
 }
